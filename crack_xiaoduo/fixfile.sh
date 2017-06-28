99507c93cd3c4b110d017c2b6decabacbe9f66d1
leonkennedy <lion0119@hotmail.com>
2017-06-28 16:33:50 +0800

    fix auth for xiaoduo

--- crack_xiaoduo/fixfile.sh
+++ crack_xiaoduo/fixfile.sh
@@ -24,8 +24,14 @@ function side_windows()
 {
     filename='lib/side_window.pyo_dis'
     param1='       self.is_show = 0'
+#hide self
     sed -i "257a\ $param1" $filename
-    sed -i 's/self.half_auto_lite_window.show/self.half_auto_lite_window.hide/g' $filename
+#hide down lite window
+    sed -i 's/self.half_auto_lite_window.show(/self.half_auto_lite_window.hide(/g' $filename
+#hide lite-side window
     sed -i '/self.side_window_lite_window.show/d' $filename
+    # crack auth
+    sed -i "s/user_info.get('day_to_expire', -1)/1/" $filename
+    sed -i "s/or check_msg_setting.get('asked', {}).get('enabled', False)//" side_window.pyo_dis
 }
 
